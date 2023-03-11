83e17c540ca6fef469e3de8d3555c4dbf617030a
tamar shear yeshov <tamar0556789257@gmail.com>
2023-03-12 01:47:26 +0200

    resume

---
+++ resume/nodejs/resume.js
@@ -0,0 +1,20 @@
+
+class resume{    
+    id;
+    name;
+    Description;
+    phone;
+    amail;
+    adress;
+    linq;
+    profil;
+    WorkExperience;
+    education;
+    projects;
+    Courses;
+    tools;
+    Languages;
+    skills;
+    Design;
+    
+}
